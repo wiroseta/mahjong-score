1f14422d9a92a1a0f96abb0e50d2f419c99d204e
wiroseta <wiroseta@gmail.com>
2026-09-24 07:02:35 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="mahjong-score-v2";
+const CACHE="mahjong-score-v4-correct-result";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
